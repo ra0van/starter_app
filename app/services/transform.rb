1df7ff769cccf96392e8eb18da0c5824ba9f1258
ra0van <rajasekhar745@gmail.com>
2024-01-29 11:11:23 +0530

    Update Indentation

--- app/services/transform.rb
+++ app/services/transform.rb
@@ -1,65 +1,65 @@
 require 'date'
 
-
 class Transform
- def transform_ads(data)
-  puts 'Transforming'
-  ads = data.map { |record|
-    {
-      id: record['id'],
-      name: record['name'],
-      adset_id: record['adset_id'],
-      account_id: record['account_id'],
+  def transform_ads(data)
+    puts 'Transforming'
+    ads = data.map { |record|
+      {
+        id: record['id'],
+        name: record['name'],
+        adset_id: record['adset_id'],
+        account_id: record['account_id'],
+      }
     }
-  }
-  puts 'Saving Records'
-  Ads.upsert_all(ads, unique_by: :id)
- end
+    puts 'Saving Records'
+    Ads.upsert_all(ads, unique_by: :id)
+  end
 
- def transform_ad_insights(data)
-   ad_insights = data.map { |insight|  
-     {
-       clicks: insight['clicks'],
-       ctr: insight['ctr'],
-       link_clicks: insight['inline_link_clicks'],
-       #link_clicks_ctr: insight['link_clicks_ctr'],
-       event_date: Date.strptime(insight['date_stop'], '%Y-%m-%d'),
-       comments: insight['comments'],
-       spend: insight['spend'],
-       ad_id: insight['ad_id'],
-       account_id: insight['account_id'],
-     }
-   }
+  def transform_ad_insights(data)
+    ad_insights = data.map { |insight|
+      {
+        clicks: insight['clicks'],
+        ctr: insight['ctr'],
+        link_clicks: insight['inline_link_clicks'],
+        #link_clicks_ctr: insight['link_clicks_ctr'],
+        event_date: Date.strptime(insight['date_stop'], '%Y-%m-%d'),
+        comments: insight['comments'],
+        spend: insight['spend'],
+        ad_id: insight['ad_id'],
+        account_id: insight['account_id']
+      }
+    }
 
-   AdsMetrics.upsert_all(ad_insights, unique_by: [:ad_id,:event_date])
-   # metrics = AdsMetrics.order('id').all
-   # metrics.each { |x| puts x.inspect }
- end
+    AdsMetrics.upsert_all(ad_insights, unique_by: %i[ad_id event_date])
+    # metrics = AdsMetrics.order('id').all
+    # metrics.each { |x| puts x.inspect }
+  end
 
- def transform_ad_sets(data)
-   adsets = data.map{ |set|
-     {
-       id: set['id'],
-       name: set['name'],
-       goal: set['optimization_goal'],
-       daily_budget: set['daily_budget'],
-       lifetime_budget: set['lifetime_budget'],
-       billing_event: set['billing_event'],
-       campaign_id: set['campaign_id'],
-       account_id: set['account_id']
-     }
-   }
+  def transform_ad_sets(data)
+    adsets = data.map{ |set|
+      {
+        id: set['id'],
+        name: set['name'],
+        goal: set['optimization_goal'],
+        daily_budget: set['daily_budget'],
+        lifetime_budget: set['lifetime_budget'],
+        billing_event: set['billing_event'],
+        campaign_id: set['campaign_id'],
+        account_id: set['account_id']
+      }
+    }
 
-  AdSets.upsert_all(adsets, unique_by: :id)
- end
+    AdSets.upsert_all(adsets, unique_by: :id)
+  end
 
   def transform_ad_accounts(data)
     ad_accounts = data.map{ |account| 
-    {
-      id: account['id'],
-      name: account['name']
+      {
+        id: account['id'],
+        name: account['name'],
+        currency: account['currency'],
 
-    }
+      }
     }
   end
 end
--- lib/tasks/extract.rake
+++ lib/tasks/extract.rake
@@ -8,22 +8,19 @@ namespace :Facebook do
 
     tr = Transform.new
 
-    #puts fb.get_ad_accounts("236247562146310")
-
-  
-    #data = fb.get_ad_campaigns("act_202330961584003")
-    data = fb.get_ad_sets("act_202330961584003")
+    # data = fb.get_ad_campaigns("act_202330961584003")
+    data = fb.get_ad_sets('act_202330961584003')
     tr.transform_ad_sets(data)
-    
-    data = fb.get_ads("act_202330961584003")
+
+    data = fb.get_ads('act_202330961584003')
     tr.transform_ads(data)
 
-    data = fb.get_ad_insights("act_202330961584003")
+    data = fb.get_ad_insights('act_202330961584003')
     tr.transform_ad_insights(data)
 
-    ActiveRecord::Base.connection.instance_variable_set :@logger, Logger.new(STDOUT)
+    ActiveRecord::Base.connection.instance_variable_set :@logger, Logger.new($stdout)
     ActiveRecord::Base.logger = nil #Logger.new(STDOUT)
 
-    #tr.transform_ad_sets(data)
+    # tr.transform_ad_sets(data)
   end
 end
