174f3dc1db3a251ffe840fee1d172cc7335a6642
ra0van <rajasekhar745@gmail.com>
2024-02-02 15:48:16 +0530

    V1 Query Parser

--- app/services/query_constructor.rb
+++ app/services/query_constructor.rb
@@ -8,6 +8,9 @@ class AdsQueryInterface
   HIERARCHY = { 'ad_accounts' => 0, 'ad_campaigns' => 1, 'ad_sets' => 2, 'ads' => 3
   }.freeze
 
+  JOIN_KEY = { 'ad_accounts' => 'id', 'ad_campaigns' => 'account_id', 'ad_sets' => 'campaign_id', 'ads' => 'adset_id', 'adaccount_metrics' => 'account_id', 'campaign_metrics' => 'campaign_id', 'adset_metrics' => 'adset_id', 'ad_metrics' => 'ad_id'
+  }.freeze
+
   # Maps entities to their corresponding metrics tables
   METRICS_ASSOCIATION = {
     'ads' => 'ad_metrics',
@@ -17,28 +20,28 @@ class AdsQueryInterface
   }.freeze
 
   def generate_query(fields, filters)
-    metrics, dimensions, levelarate_fields(fields)
+    metrics, dimensions, level = separate_fields(fields)
     metrics = prefix_metrics(metrics, level)
-    join(metrics, dimensions, level)
-    # Join LD & LM
-    # Join Other Dimensions
+    query = construct_sql_query(metrics, dimensions, level)
+    puts query
     # Add Date filter
     # Add Account filter
   end
 
   def separate_fields(fields)
-    metrics = [], dimensions = []
-    lowest_dim_index = 999
+    metrics = []
+    dimensions = []
+    lowest_dim_index = -1
     fields.each do |field|
       if METRICS_COLUMNS.include? field
-        metrics << field
+        metrics.push(field)
       else
-        dimensions << field
+        dimensions.push(field)
         node, = field.split('.')
-        lowest_dim_index = HIERARCHY[node] > lowest_dim_index ? HIERARCHY[node] : lowest_dim_index
+        puts node.inspect
+        lowest_dim_index = [HIERARCHY[node], lowest_dim_index].max
       end
     end
-
     [metrics, dimensions, HIERARCHY.key(lowest_dim_index)]
   end
 
@@ -47,24 +50,32 @@ class AdsQueryInterface
     fields.map { |field| "#{prefix}.#{field}" }
   end
 
-  def join(metrics, dimensions, level)
+  def construct_sql_query(metrics, dimensions, level)
     nodes = dimensions.map do |item|
       entity, = item.split('.')
       entity
     end
     # Step 2: Sort by hierarchy value in descending order and remove duplicates
     nodes = nodes.uniq.sort_by { |item| - HIERARCHY[item] }
-    
-    if !metrics.empty?
-      
 
-  end
+    table = metrics.empty? ? nodes.shift : METRICS_ASSOCIATION[level]
 
-  def get_lowest_node(tables, hierarchy)
-    level = tables.max_by do |table|
-      hierarchy[table] || 999
+    # TODO : If Join is not incremental, then fill the blanks before joining
+    join_query = ["FROM #{table}"]
+    prev = table
+    nodes.each do |node|
+      join_query.push("INNER JOIN #{node} on #{prev}.#{JOIN_KEY[prev]} = #{node}.id")
+      prev = node
     end
-    puts level
+
+    select_query = ['SELECT']
+    metrics.each do |metric|
+      select_query.push("#{metric},")
+    end
+    dimensions.each do |dimension|
+      select_query.push("#{dimension},")
+    end
+
+    select_query.join(' ').chop + ' ' + join_query.join(' ')
   end
 end
-
--- lib/tasks/query.rake
+++ lib/tasks/query.rake
@@ -6,10 +6,14 @@ namespace :Facebook do
   desc 'Query Dimension and metrics'
   task run_query: :environment do
     selected_fields = ['ads.name','ad_sets.goal','ads.type', 'ad_sets.name', 'ad_campaigns.name', 'clicks']
-    filters = { 'ad_sets.name' => 'Example Ad Set', 'ad_accounts.id' => '12312' }
+    filters = { 'ad_sets.name' => 'Example Ad Set', 'ad_accounts.id' => '202330961584003' }
     q = AdsQueryInterface.new
     query = q.generate_query(selected_fields, filters)
+
+    puts "============"
+    selected_fields = ['ads.name','ad_sets.goal','ads.type', 'ad_sets.name', 'ad_campaigns.name']
+    query = q.generate_query(selected_fields, filters)
+
     # puts query
   end
 end
-
